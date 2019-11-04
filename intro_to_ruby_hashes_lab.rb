4054c8c8856b3a7f4b8cb046b37414e109d6c9eb
Anthony Molina <antonio-molina@att.net>
2019-11-04 20:20:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,8 +8,8 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-   monopoly = { :railroads => { :pieces => 4 },  }
-         {:rent_in_dollars}
+   monopoly = { :railroads => { :pieces => 4 }, {:rent_in_dollars} }
+         
         #:names =>
           #{:key1 => {}, 
            #:key2 => {}, 
