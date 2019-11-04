4a4381d32ce8d796e2fc5b42c7b7b4812da2ae8f
Anthony Molina <antonio-molina@att.net>
2019-11-04 21:05:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ monopoly = { :railroads =>
         :four_pieces_owned => 200 }, 
       :names => 
         {:reading_railroad => {"mortgage_value" => "$100"}, 
-         :pennsylvania_railroad => {}, 
+         :pennsylvania_railroad => {"mortgage_value" => "$200"}, 
          :b_and_o_railroad => {}, 
          :shortline_railroad => {} },
         
