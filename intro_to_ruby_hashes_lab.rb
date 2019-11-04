172aa13c76d77de0f02863e36036f9bfa4cb113c
Anthony Molina <antonio-molina@att.net>
2019-11-04 20:17:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,26 +1,46 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+  monopoly = { :railroads => {} } 
+	
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  monopoly = { :railroads => { :pieces => 4 } }
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+   monopoly = { :railroads => { :pieces => 4 } }
+        #:names =>
+          #{:key1 => {}, 
+           #:key2 => {}, 
+           #:key3 => {}, 
+           #:key4 => {} },  
+      #{:rent_in_dollars =>
+       # { :one => {}, 
+        #  :two => {}, 
+         # :three => {}, 
+          #:four => {} }
+      #}    
+     #}    
+                   
+   #} 
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+# { :railroads => 
+   
+   # { :rent_in_dollars => 
+         #{  :one_piece_owned => 25,
+         # :one => {}
+          # :two => {}, 
+          #:three => {}, 
+          #:four => {} },
+          
+     #:names => 
+      #  { :pieces => 4, 
+       #    :key2 => {}, 
+        #   :key3 => {}, 
+         #  :key4 => {} }
+           
+    #}
+ #}  
 end
