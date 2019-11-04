a13f79b10153e765ccc9ae7b39a06832a0891705
Anthony Molina <antonio-molina@att.net>
2019-11-04 20:45:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ def monopoly_with_third_tier
  monopoly = { :railroads => 
   { :pieces => 4, 
     :rent_in_dollars =>
-      { :one => {}, 
+      { :one_piece_owned => 25, 
         :two => {}, 
         :three => {}, 
         :four => {} }, 
